8e8c18826500fb0685e3eef65f06136387791779
hellohellohell012321 <hhellohellohell012321@gmail.com>
2026-05-14 22:03:23 -0700

    Update impossible_squid_game.lua

--- scripts/game_stuff/impossible_squid_game.lua
+++ scripts/game_stuff/impossible_squid_game.lua
@@ -2,10 +2,16 @@ local segments = workspace:FindFirstChild("segmentSystem"):FindFirstChild("Segme
 
 for _, segment in ipairs(segments) do
     for _, part in ipairs(segment.Folder:GetChildren()) do
+        
         part.CanCollide = true
         part.CanTouch = false
+        
         for _, i in ipairs(part:GetChildren()) do
+            if i.Name == "breakable" then
+                part.Color = Color3.fromRGB(0, 0, 0)
+            end
             i:Destroy()
         end
+        
     end
 end
